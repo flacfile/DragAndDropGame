0498045ee7f47fe1545f7326cc46a3bf50df83a7
Jegors Kalejs <jegors.kalejs@gmail.com>
2023-05-24 12:18:53 +0300

    Iesakta drag drop skripta rakstisana

---
+++ Assets/Skripti/DragAndDropSkripts.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropSkripts : MonoBehaviour,
+    IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    public Objekti objektuSkripts;
+    private CanvasGroup kanvasGrupa;
+    private RectTransform velkObjRectTransf;
+
+
+    void Start()
+    {
+        kanvasGrupa = GetComponent<CanvasGroup>();
+        velkObjRectTransf = GetComponent<RectTransform>();
+    }
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+    }
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
+
+}
---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Objekti : MonoBehaviour,
+	IBeginDragHandler, IDragHandler, IEndDragHandler {
+	public Objekti objektuSkripts;
+	private CanvasGroup kanvasGrupa;
+	private RectTransform velkObjRectTransf;
+
+
+	void Start ()
+	{
+		kanvasGrupa= GetComponent<CanvasGroup>();
+		velkObjRectTransf= GetComponent<RectTransform>();
+	}
+
+
+	public void OnBeginDrag(PointerEventData eventData)
+	{
+
+	}
+
+
+	public void OnDrag(PointerEventData eventData)
+	{
+
+	}
+
+	public void OnEndDrag(PointerEventData eventData)
+	{
+
+	}
+	
+
+}
